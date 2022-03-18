6bbd94af0c6d94bc82fe61e9b858960e2c42cdc2
GloNyakundi <99646401+GloNyakundi@users.noreply.github.com>
2022-03-18 12:52:10 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  printf("nthis is main file)
+         }
+         
+         void print()
+         {
+           printf("nthis is print function
+                  }
+     
